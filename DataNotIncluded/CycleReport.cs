7338401e2ba53d63ec1dc9fce662f69c9684cc54
Ajoor997 <50848501+Ajoor997@users.noreply.github.com>
2021-08-06 20:39:25 -0500

    Add project files.

---
+++ DataNotIncluded/CycleReport.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+
+
+namespace DataNotIncluded
+{
+    class CycleReport
+    {
+        private DataOject gameData;
+
+        public void ExtractData()
+        {
+            // Handling Headers
+            //this.gameData.dataHeader;
+            //this.gameData.dataRows;
+
+        }
+        public CycleReport(DataOject gameData)
+        {
+            this.gameData = gameData;
+        }
+    }
+
+    public struct Context {
+
+        public string rowHeader;
+        public float Positive;
+        public float Negative;
+        public float Net;
+        public List<Note> ContextNotes;
+
+        public void AddNote(Note value)
+        {
+            this.ContextNotes.Add(value);
+        }
+
+        public Context(string rowHeader, float Positive, float Negative, float Net)
+        {
+            this.rowHeader = rowHeader;
+            this.Positive = Positive;
+            this.Negative = Negative;
+            this.Net = Net;
+            this.ContextNotes = new List<Note>();
+        }
+    }
+
+    public struct Note
+    {
+        public string label;
+        public float value;
+
+        public Note(string label, float value)
+        {
+            this.label = label;
+            this.value = value;
+        }
+    }
+
+    public struct ToolTip
+    {
+        public string stringValue;
+        public ReportManager.ReportEntry.Order order;
+        public ToolTip(string stringValue, ReportManager.ReportEntry.Order order)
+        {
+            this.stringValue = stringValue;
+            this.order = order;
+        }
+    }
+    public struct DataOject
+    {
+        public DataHeader dataHeader;
+        public List<DataRow> dataRows;
+
+
+        public DataOject(DataHeader dataHeader, List<DataRow> dataRows)
+        {
+            this.dataHeader = dataHeader;
+            this.dataRows = dataRows;
+        }
+    }
+
+    public struct DataRow {
+
+        public ReportManager.ReportType type;
+        public float added;
+        public float removed;
+        public float net;
+        public int cycle;
+        public List<Context> rowContexts;
+        public List<Note> notes;
+
+
+        public void AddRowContext(Context value)
+        {
+            this.rowContexts.Add(value);
+        }
+        public void AddNote(Note value)
+        {
+            this.notes.Add(value);
+        }
+
+        public DataRow(int cycle, ReportManager.ReportType type, float added, float removed, float net)
+        {
+            this.cycle = cycle;
+            this.type = type;
+            this.added = added;
+            this.removed = removed;
+            this.rowContexts = new List<Context>();
+            this.notes = new List<Note>();
+            this.net = net;
+        }    
+    }
+
+    public struct DataHeader
+    {
+        public string header;
+        public ToolTip positiveTooltip;
+        public ToolTip negativeTooltip;
+
+        public DataHeader(string header, ToolTip positiveTooltip, ToolTip negativeTooltip)
+        {
+            this.header = header;
+            this.positiveTooltip = positiveTooltip;
+            this.negativeTooltip = negativeTooltip;
+        }
+
+    }
+}
---
+++ DataNotIncluded/DataNotIncludedPatches.cs
@@ -0,0 +1,108 @@
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine.Events;
+
+
+namespace DataNotIncluded
+{
+    public class DataNotIncludedPatches
+    {
+        // Append Export DataButton 
+        
+        [HarmonyPatch(typeof(PauseScreen), "OnPrefabInit")]
+        public class DataNotIncludedExporter
+        {
+            private static void Postfix(PauseScreen __instance)
+            {
+                // Init
+                Debug.Log("[DataNotIncluded] : Export Button Initialized!");
+                FieldInfo buttonsObjectVariable = AccessTools.Field(typeof(KButtonMenu), "buttons");
+
+                // Get List of Menu Buttons
+                List<KButtonMenu.ButtonInfo> pauseMenuButtons = new List<KButtonMenu.ButtonInfo>(
+                    (IEnumerable<KButtonMenu.ButtonInfo>) buttonsObjectVariable.GetValue(__instance));
+                
+                // Create the Button
+                KButtonMenu.ButtonInfo ExportBtn = new KButtonMenu.ButtonInfo(
+                    "Export Daily Reports",
+                    (Action)266,
+                    new UnityAction(OnExportDailyReports));
+
+                // Append the new button
+                pauseMenuButtons.Insert(pauseMenuButtons.Count-1, ExportBtn);
+                buttonsObjectVariable.SetValue(__instance, pauseMenuButtons);
+
+                // Reset
+                buttonsObjectVariable = null;
+            }
+
+
+
+            private static void OnExportDailyReports()
+            {
+                // Init
+                Debug.Log("[DataNotIncluded] : Exporting Daily Reports");
+                List<DataRow> dataRows = new List<DataRow>();
+                DataHeader dataGroupHeaders = new DataHeader();
+                DataOject finalComposedData = new DataOject();
+
+                // GameObjects
+                List<ReportManager.DailyReport> reports = ReportManager.Instance.reports;
+                Dictionary<ReportManager.ReportType, ReportManager.ReportGroup> reportDict = ReportManager.Instance.ReportGroups;
+
+                // Get TooltipHeader
+                foreach (var reportGroup in reportDict)
+                { 
+                    ToolTip positiveTooltip = new ToolTip(reportGroup.Value.positiveTooltip, reportGroup.Value.posNoteOrder);
+                    ToolTip negativeTooltip = new ToolTip(reportGroup.Value.negativeTooltip, reportGroup.Value.posNoteOrder);
+                    dataGroupHeaders = new DataHeader(reportGroup.Value.stringKey, positiveTooltip, negativeTooltip);
+                }
+
+                // Get Actual Data
+                if (reports != null) { // Not Empty
+
+                    reports.ForEach(delegate (ReportManager.DailyReport currentCycleReport)
+                    {
+                        Debug.Log("[DataNotIncluded] : DAY: " + currentCycleReport.day);
+
+                        currentCycleReport.reportEntries.ForEach(delegate (ReportManager.ReportEntry entry) {
+                            DataRow dataRow = new DataRow(currentCycleReport.day, entry.reportType, entry.Positive, entry.Negative, entry.Net);
+
+                            if (entry.HasContextEntries()) {
+                                for (int index = 0; index < entry.contextEntries.size; index++)
+                                {
+                                    Context rowContext = new Context(entry.contextEntries[index].context,
+                                        entry.contextEntries[index].Positive,
+                                        entry.contextEntries[index].Negative,
+                                        entry.contextEntries[index].Net);
+                                    // Get Corresponding Notes
+                                    entry.contextEntries[index].IterateNotes(delegate (ReportManager.ReportEntry.Note note)
+                                    {
+                                        Note rowNote = new Note(note.note, note.value);
+                                        rowContext.AddNote(rowNote);
+                                    });
+                                    dataRow.AddRowContext(rowContext);
+                                }
+                            }
+                            // Get Corresponding Notes
+                            entry.IterateNotes(delegate (ReportManager.ReportEntry.Note note)
+                            {
+                                Note reportTypeNote = new Note(note.note, note.value);
+                                dataRow.AddNote(reportTypeNote);
+                            });
+                            // Append
+                            dataRows.Add(dataRow);
+                        });
+                    }); 
+                    finalComposedData= new DataOject(dataGroupHeaders, dataRows);
+                }
+                reports = null;
+                reportDict = null;
+                // Class Init
+                CycleReport dataExtractor = new CycleReport(finalComposedData);
+                dataExtractor.ExtractData();
+            }
+        }
+    }
+}
